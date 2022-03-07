7cd5281fb5fae01a03fc0bad4ec2d7d0cde9d8b0
’不知道取什么名字好‘ <1431861210@qq.com>
2022-03-07 20:06:41 +0800

    ’2022/3/7‘

---
+++ src/network/request.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export function request(config) {
+  // 方法1.创建aios的实例
+  const instance = axios.create({
+    baseURL: 'http://localhost:8080',
+    timeout: '10000',
+  })
+  // 2.axios的拦截器
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+    // console.log(err);
+  })
+  //2.2 响应拦截
+  instance.interceptors.response.use(res => {
+    console.log(res); //获取结果 
+    return res.data
+  }, err => {
+    console.log(err); //获取报错信息
+  })
+  //3.发送真正的网络请求
+  return instance(config)
+}
+
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1>首页</h1>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
-export default { 
-  name:"Home"
+import NavBar from 'components/common/navbar/NavBar.vue';
+import {getHomeMultidata} from 'network/home'
+
+export default {
+  name:"Home",
+  components:{
+    NavBar
+  },
+  created(){
+    //1.请求多个数据
+    getHomeMultidata().then(res =>{
+      console.log(res);
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: var(--color-tint);
+  color: aliceblue;
+}
 </style>
