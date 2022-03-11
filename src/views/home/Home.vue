6d860217090fd9e17af157e9d19c683c87513942
zhouqua <1431861210@qq.com>
2022-03-11 16:25:54 +0800

    16:25

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar.vue';
+import HomeSwiper from './chlidComps/HomeSwiper.vue'
+import HomeRecommendView from './chlidComps/HomeRecommendView.vue';
 import {getHomeMultidata} from 'network/home'
+
+
 
 export default {
   name:"Home",
   components:{
-    NavBar
+    NavBar,
+    HomeSwiper,
+    HomeRecommendView,
   },
   data(){
     return{
-      result:null
+      banners:[],
+      recommends:[]
     }
   },
   created(){
     //1.请求多个数据
     getHomeMultidata().then(res =>{
-      console.log(res);
-      this.result = res
+      // console.log(res);
+     this.banners = res.data.banner.list;
+    //  console.log(this.banners);
+     this.recommends = res.data.recommend.list;
+    //  console.log(this.recommends);
     })
   }
 }
---
+++ src/views/home/chlidComps/HomeRecommendView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="div-img">
+      <a :href="item.link">
+        <img :src='item.image' alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default ({
+  name:'HomeRecommendVieww',
+  props:{
+    recommends:{
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  },
+})
+</script>
+<style scoped>
+.recommend{
+ display: flex;
+}
+.div-img{
+ flex: 1;
+ text-align:center;
+}
+img{
+  width: 75px;
+  height: 75px;
+}
+</style>
